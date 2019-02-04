a4e1045095c4f3b2e4c42e47b1e3347f9f4adda8
EstebanVictorio <34945296+EstebanVictorio@users.noreply.github.com>
2019-02-03 21:54:50 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,2 +1,4 @@
-const Home = props => <div>Welcome to the Pokedex home page!</div>
+const Home = props => <div>Welcome to the Pokedex home page!
+  <a href="www.google.com">Go to Google Here</a>
+  </div>
 export default Home
